4d6b3d3b7ef9569fee05b8d58747340a4afeab9f
Nicole Thompson Gonzalez <gavago@gmail.com>
2020-02-09 16:32:01 -0700

    create shell for permutation script

---
+++ 2a. create indices from randomized count data.R
@@ -0,0 +1,7 @@
+
+
+# within years, randomly sample and replace nodes of networks
+# create 1000 randomized versions of 2009 - 2017 networks
+# save to extract network measures on
+# turn network measures into distrubtions to then test for signicance in e.g. integration in a given year,
+# changes in integration from year to year, etc.
