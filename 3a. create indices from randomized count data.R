0501c7f7855655e2c48b50783fbd4aa67563ff77
Nicole Thompson Gonzalez <gavago@gmail.com>
2020-02-18 14:27:36 -0700

    fixes IDs for randomized sna measures

--- 3a. create indices from randomized count data.R
+++ 3a. create indices from randomized count data.R
@@ -7,25 +7,31 @@ library(igraph)
 # turn network measures into distrubtions to then test for signicance in e.g. integration in a given year,
 # changes in integration from year to year, etc.
 
+load("data/attribute data alone.Rdata")
 load("data/indices - annual dyadic grooming.Rdata", verbose = T)
 load("data/indices - annual dyadic 5m proximity.Rdata", verbose = T)
+load("functions/functions - data preparation.Rdata", verbose = T)
 load("functions/functions - SNA measures and graph plotting.Rdata", verbose = T)
+load("data/sna graphs -  name & sna measures as vector attributes, index as edge weight.Rdata", verbose = TRUE)
 
 
 #  Randomized undirected graphs -------
+list_ran_sna_measure_df <- vector("list", length = 1000)
 
 # do this 1000 f'ing times
 
-
+for(j in seq(list_ran_sna_measure_df)){
+  
+  
 ran_gdf_gmgmd_sex_comb <- total_gm_gmd_index %>%
   #sample/randomize individuals within years
   group_by(year) %>%
-  mutate(RID1 = sample(ID1)) %>%
+  mutate(RID1 = sample(ID1), RID2 = sample(ID2)) %>%
   ungroup() %>%
   # nest dyadic data in list column (o.g. data script 3.3)
   mutate(dyad_sex = "any_combo") %>%
-  select(year, dyad_sex, ID1,ID2, gmgmdi) %>%
-  nest(data = c(ID1,ID2, gmgmdi)) %>%
+  select(year, dyad_sex, RID1, RID2, gmgmdi) %>%
+  nest(data = c(RID1, RID2, gmgmdi)) %>%
   arrange(year) %>%
   # turn list col dyadic data into graphs (o.g. data in script 4.1)
   mutate(graph = map(data, function(x) graph_from_data_frame(d = x, directed = FALSE))) %>%
@@ -35,12 +41,12 @@ ran_gdf_gmgmd_sex_comb <- total_gm_gmd_index %>%
 ran_gdf_gmgmd_sex_sep <- total_gm_gmd_index %>%
   #sample/randomize individuals within years
   group_by(year) %>%
-  mutate(RID1 = sample(ID1)) %>%
+  mutate(RID1 = sample(ID1), RID2 = sample(ID2)) %>%
   ungroup() %>%
   # nest dyadic data in list column (o.g. data script 3.3)
   mutate(dyad_sex != "mixed") %>%
-  select(year, dyad_sex, ID1,ID2, gmgmdi) %>%
-  nest(data = c(ID1,ID2, gmgmdi)) %>%
+  select(year, dyad_sex, RID1, RID2, gmgmdi) %>%
+  nest(data = c(RID1, RID2, gmgmdi)) %>%
   arrange(year) %>%
   # turn list col dyadic data into graphs (o.g. data in script 4.1)
   mutate(graph = map(data, function(x) graph_from_data_frame(d = x, directed = FALSE))) %>%
@@ -50,12 +56,12 @@ ran_gdf_gmgmd_sex_sep <- total_gm_gmd_index %>%
 ran_gdf_prox_sex_comb <- index_5m %>%
   #sample/randomize individuals within years
   group_by(year) %>%
-  mutate(RID1 = sample(ID1)) %>%
+  mutate(RID1 = sample(ID1), RID2 = sample(ID2)) %>%
   ungroup() %>%
   # nest dyadic data in list column (o.g. data script 3.3)
   mutate(dyad_sex = "any_combo") %>%
-  select(year, dyad_sex, ID1,ID2, prox5i) %>%
-  nest(data = c(ID1,ID2, prox5i)) %>%
+  select(year, dyad_sex, RID1, RID2, prox5i) %>%
+  nest(data = c(RID1, RID2, prox5i)) %>%
   arrange(year) %>%
   # turn list col dyadic data into graphs (o.g. data in script 4.1)
   mutate(graph = map(data, function(x) graph_from_data_frame(d = x, directed = FALSE))) %>%
@@ -65,32 +71,27 @@ ran_gdf_prox_sex_comb <- index_5m %>%
 ran_gdf_prox_sex_sep <- index_5m %>%
   #sample/randomize individuals within years
   group_by(year) %>%
-  mutate(RID1 = sample(ID1)) %>%
+  mutate(RID1 = sample(ID1), RID2 = sample(ID2)) %>%
   ungroup() %>%
   # nest dyadic data in list column (o.g. data script 3.3)
   mutate(dyad_sex != "mixed") %>%
-  select(year, dyad_sex, ID1,ID2, prox5i) %>%
-  nest(data = c(ID1,ID2, prox5i)) %>%
+  select(year, dyad_sex, RID1, RID2, prox5i) %>%
+  nest(data = c(RID1, RID2, prox5i)) %>%
   arrange(year) %>%
   # turn list col dyadic data into graphs (o.g. data in script 4.1)
   mutate(graph = map(data, function(x) graph_from_data_frame(d = x, directed = FALSE))) %>%
-  #add sna attributes to vertices
+  # add sna attributes to vertices
   mutate(graph_w_sna = map(graph, sna_measures_undir, network_sex = dyad_sex, output = "graph"))
-
-
-
-
-
 
 
 #create master data frame with all individual sna measures by year
 
-graph_list <- list(gdf_gm_sex_sep, gdf_gm_sex_comb, gdf_prox_sex_sep, gdf_prox_sex_comb) 
+ran_graph_list <- list(ran_gdf_gmgmd_sex_sep, ran_gdf_gmgmd_sex_comb, ran_gdf_prox_sex_sep, ran_gdf_prox_sex_comb) 
 
-all_sna_measure_df_list <- vector("list", length(graph_list))
+ran_sna_measure_df_list <- vector("list", length(ran_graph_list))
 
-for(j in 1:length(graph_list)){
-  gdf <- graph_list[[j]] 
+for(j in 1:length(ran_graph_list)){
+  gdf <- ran_graph_list[[j]] 
   n <- nrow(gdf)
   measures_list <- vector("list", length = n)
   
@@ -101,12 +102,18 @@ for(j in 1:length(graph_list)){
     measures_list[[i]] <- sna_measures_undir(g, year = y, network_sex = s, output = "data.frame")
   }
   df <- do.call("rbind", measures_list)
-  all_sna_measure_df_list[[j]] <- df
+  ran_sna_measure_df_list[[j]] <- df
 }
 
+ran_sna_measure_df <- do.call("rbind", ran_sna_measure_df_list) %>%
+  left_join(attr %>% select(chimp_id, sex, ends_with("id"), dobc, dfs, dls), by = "chimp_id") %>%
+  add_age(dyad = FALSE) %>%
+  arrange(year, network_sex, behavior, chimp_id)
 
 
+list_ran_sna_measure_df[[j]] <- ran_sna_measure_df
 
+}
 
 
 # gyard -----------
