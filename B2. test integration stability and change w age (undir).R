99b7ae826e6030706374d4545d0f801efcb7cbd4
Nicole Thompson Gonzalez <gavago@gmail.com>
2020-02-20 18:30:27 -0700

    moves descriptives and calcs sig hi lo stability

--- B2. test integration stability and change w age (undir).R
+++ B2. test integration stability and change w age (undir).R
@@ -23,160 +23,91 @@ load("data/attribute data alone.Rdata", verbose = T)
 # stability/variation in sna measures (adjusted for observation time)
 # magnitude of various sna variables
 
-# i. Descriptives and Focal subjects for age analysis ----
-names(all_sna_measure_df)
-unique(all_sna_measure_df$network_sex)
-unique(all_sna_measure_df$behavior)
-
-# relevant ages
-final_ages <- attr %>%
-  transmute(chimp_id = chimp_id, age_mid_2018 = as.numeric(as.Date("2018-06-01") - dobc)/365.25, age_last_seen = as.numeric(dls - dobc)/365.25)
-final_ages
-
-# mean n sd of sna measures by what sexes are included in the network
-all_sna_measure_df %>%
-  filter(behavior == "total_grooming") %>%
-  filter(year != 2009) %>%
-  group_by(network_sex) %>%
-  summarise_at(vars(bt, ec, deg, trans), .funs = list(mean = mean, sd = sd))
-
-# how many years individs present in focal data  
-years_pres <- all_sna_measure_df %>%
-  filter(behavior == "total_grooming") %>%
-  filter(year != 2009) %>%
-  filter(network_sex == "any_combo") %>%
-  group_by(chimp_id) %>%
-  tally() %>%
-  ungroup()
-
-years_pres
-
-#identify priority individuals for OS - those who are oldest in the middle of sample collection 
-#and simultaneously have been focal followed longest.
-priorities_alive_2018 <- years_pres %>%
-  left_join(final_ages, by = "chimp_id") %>%
-  filter(is.na(age_last_seen)) %>%
-  arrange(desc(n), desc(age_mid_2018)) %>%
-  filter(n == 8) %>%
-  pull(chimp_id)
-priorities_alive_2018
-
-
-years_pres %>%
-  left_join(final_ages, by = "chimp_id") %>%
-  filter(!is.na(age_last_seen)) %>%
-  arrange(desc(n), desc(age_last_seen)) %>%
-  filter(age_last_seen > 40) %>%
-  pull(chimp_id)
-
-priorities %>%
-  filter(n == 8) %>%
-  summarise( mean = mean(age_mid_2018), sd = sd(age_mid_2018), min = )
-
-
-# ii. Look at very simple correlations w age within networks (no RE for individual...) -----
-
-unique(all_sna_measure_df$network_sex)
-unique(all_sna_measure_df$network_type)
-
-# female
-all_sna_measure_df %>%
-filter(network_sex == "female", behavior == "total_grooming") %$% 
-  cor.test(trans, age_mid_year) # strong neg ec, bt, deg
-  
-all_sna_measure_df %>%
-  filter(network_sex == "female", behavior == "prox") %$% 
-  cor.test(trans, age_mid_year) #nada
-
-# male
-all_sna_measure_df %>%
-  filter(network_sex == "male", behavior == "total_grooming") %$% 
-  cor.test(deg, age_mid_year) # pos ec
-
-all_sna_measure_df %>%
-  filter(network_sex == "male", behavior == "prox") %$% 
-  cor.test(trans, age_mid_year) #nada
-
-# all
-#group this by male and female within full network
-all_sna_measure_df %>%
-  filter(network_sex == "any_combo", behavior == "total_grooming") %$% 
-  cor.test(trans, age_mid_year) #nada
-
-all_sna_measure_df %>%
-  filter(network_sex == "any_combo", behavior == "prox") %$% 
-  cor.test(trans, age_mid_year) #nada 
-
-# iii. basic correlations between network measures ----
-library(ggcorrplot)
-
-names(all_sna_measure_df)
-
-df <- all_sna_measure_df
-df <- all_sna_measure_df %>%
-  filter(network_sex == "female", behavior == "total_grooming") 
-df <- all_sna_measure_df %>%
-  filter(network_sex == "male", behavior == "total_grooming") 
-df <- all_sna_measure_df %>%
-  filter(network_sex == "female", behavior == "prox") 
-df <- all_sna_measure_df %>%
-  filter(network_sex == "male", behavior == "prox") 
-
-
-# total
-cors <- df %>%
-  select(bt, ec, deg, trans) %>%
-  cor(., method = "spearman" , use = "pairwise.complete.obs") %>%
-  round(., 2)
-p_cors <- df %>%
-  select(bt, ec, deg, trans) %>%
-  cor_pmat(., method = "spearman") %>%
-  round(.,2)
-1 * (p_cors < 0.05)
-1 * (cors > 0 & p_cors < 0.05)
-
-cors
-p_cors
-#in every network, all measures are positively and significantly correlated except for trans and bt 8|
-
-
-# corrs differ for prox and gm networks
-# 2a. create heat map of corr
-  
-
-
-# 1. CVs
-
-all_sna_measure_df %>%
-  filter(behavior == "total_grooming") %>%
-  filter(year != 2009) %>%
-  group_by(network_sex) %>%
-  summarise_at(vars(bt, ec, deg, trans), .funs = list(mean = mean, sd = sd))
-
-# iv. PCA how do measures load together or apart?
-
 
 # 1. Calc observed individ CVs by network sex and behavior
 cv_obs <- all_sna_measure_df %>%
-  group_by(chimp_id, network_sex, behavior) %>%
+  group_by(chimp_id, sex, network_sex, behavior) %>%
   summarise(cv_bt = sd(bt)/mean(bt)*100, 
             cv_ec = sd(ec)/mean(ec)*100, 
             cv_deg = sd(deg)/mean(deg)*100,
             cv_trans = sd(trans)/mean(trans)*100) %>%
   ungroup() %>%
-  select(chimp_id, network_sex, behavior, starts_with("cv"))
+  select(chimp_id, sex, network_sex, behavior, starts_with("cv")) %>%
+  pivot_longer(cols = starts_with("cv"), names_to = "CV_type", values_to = "obs_CV") %>%
+  ADD years obs
+  ADD attr
+  add final age (make final age)
+
+nrow(cv_obs) # 620 chimp-network cvs
+
+save(cv_obs, file = "data/ cv")
 
 
-# compare
+# test for sig stability ------
 load("data/randomized distributions of sna CVs.Rdata", verbose = T)
-# what proportion
 
-#mixed sex bt
-msgmgmd <- ran_cv_dists %>% filter(network_sex == "any_combo", behavior == "total_grooming")
+# massive case when? to create sig_high (prop dist below) and sig_low (prop dist below)
+# or make sep network sex - behavior files 
+#agh, GATHER, make one col CV type, then compare to say whether cv for that sna measure is sig stable/wonky
+
+names(cv_obs)
+
+#CV indicates how much integration varies from year to year in particular type of network
+#the higher the CV, the lower the stability of integration
+#the lower the CV, the higher the stability of integration
+
+# an obs CV that is lower than 95% observed, means sig high stability
+hi_stab_fun <- function(x){ 
+  prop <- sum(obs_cv < x$CV)/length(x$CV)
+  return(prop)
+}
+
+# an obs CV that is higher than 95% observed, mean sig low stability
+lo_stab_fun <- function(x){
+  prop <- sum(obs_cv > x$CV)/length(x$CV)
+  return(prop)
+}
 
 
-# female prox
-# female groom
-# male prox
-# male groom
+sig_stability <- cv_obs %>%
+  left_join(., ran_cv_dists_long, by = c("network_sex", "behavior", "CV_type")) %>%
+  mutate(stab_hi = map(distributions, hi_stab_fun)) %>%
+  mutate(stab_lo = map(distributions, lo_stab_fun)) %>%
+  unnest(c(stab_hi, stab_lo))
+names(sig_stability)
 
+
+#does sig hi stability occur more often in particular networks?
+sig_stability %>%
+  count(sex, network_sex, behavior) %>%
+  filter(stab_hi < 0.05) %>%
+  group_by(sex, network_sex, behavior) %>%
+  mutate()
+# where does non sig stability occur?
+sig_stability %>%
+  filter(stab_hi > 0.05) %>%
+  count(network_sex, behavior)
+
+#no one has sig lo stability :-/
+sig_stability %>%
+  filter(stab_lo < 0.05)
+
+#ok, who at least is stable more often than others
+sig_stability %>%
+  add_count(chimp_id) %>%
+  filter(stab_hi < 0.05) %>%
+  group_by(chimp_id) %>%
+  mutate(n_sig = n(), prop_sig = n_sig/n) %>%
+  ungroup() %>%
+  filter()
+  
+
+
+
+#gyard ----
+filt_cv <- function(data, net_sex, cv, beh) {
+  val <- data %>%
+    filter(network_sex == net_sex, CV_type == cv, behavior == beh) %>% 
+    pull(value)
+  return(val)
+}
