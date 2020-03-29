b3cb0767d290766402cdcc16fa2963ac83cd4eef
Nicole Thompson Gonzalez <gavago@gmail.com>
2020-03-29 10:32:55 -0600

    adds rank class

--- A7. rank attributes.R
+++ A7. rank attributes.R
@@ -13,21 +13,35 @@ which(is.na(m_ranks$Date))
 
 format(m_ranks$Date)
 
-m_ranks %>%
+m_ranks %<>%
   mutate(Date = mdy(Date), year = year(Date)) %>% #as.Date(Date, format = "%m/%d/%y"))
   group_by(year, ID) %>%
   summarise(avg_rank = mean(Rank.Proportion)) %>%
   ungroup() %>%
   group_by(year) %>%
-  mutate(rank_class = quantcut(avg_rank, 3)) %>%
-  ungroup()
-ADD LO MED HI rank based on tertiles of annual std ranks
+  mutate(rank_class = as.numeric(quantcut(avg_rank, 3))) %>%
+  ungroup() %>%
+  mutate(rank_class = as.factor(case_when(
+    rank_class == 1 ~ "lo",
+    rank_class == 2 ~ "med",
+    rank_class == 3 ~ "hi"
+  )))
+
 
 f_ranks %<>%
   mutate(Date = mdy(Date), year = year(Date)) %>%
   group_by(year, ID) %>%
   summarise(avg_rank = mean(Rank.Proportion)) %>%
-  ungroup()
+  ungroup() %>%
+  group_by(year) %>%
+  mutate(rank_class = as.numeric(quantcut(avg_rank, 3))) %>%
+  ungroup() %>%
+  mutate(rank_class = as.factor(case_when(
+    rank_class == 1 ~ "lo",
+    rank_class == 2 ~ "med",
+    rank_class == 3 ~ "hi"
+  )))
+
 
 ann_ranks <- rbind(m_ranks, f_ranks)
 
