94d1431856cfbe0f768fb04946768ae98025f945
Nicole Thompson Gonzalez <gavago@gmail.com>
2020-04-14 13:34:00 -0600

    add prop cyc and raink, change extract coef for any model type, fix count zeros in peep model data, update model when no max grad converge

--- functions/functions - age sex modeling.R
+++ functions/functions - age sex modeling.R
@@ -1,64 +1,16 @@
-# -- age - sex functions -------
 
 
-# modeling function for single sna measure ------
-age_sex_fun_single <- function(data, sna_measure = c("bt", "ec", "deg", "trans", "deg_in", "deg_out"), 
-                               beh = c("total_grooming", "prox"), 
-                               net_sex = c("any_combo", "female", "male"), 
-                               subj_sex = NULL,
-                               sex_age_int = FALSE, summary = FALSE){
-  
-  #scale shorthand function
-  z. <- function(x) scale(x)
-  #model expressions
-  #both sexes 
-  if(all(net_sex == "any_combo" & subj_sex == "both" & sex_age_int == TRUE)){
-    f <- expr(!!sym(sna_measure) + 0.00001 ~ z.(age_mid_year) + sex + z.(avg_rank) + sex*z.(age_mid_year) + (1|chimp_id))
-  }
-  if(all(net_sex == "any_combo" & subj_sex == "both" & sex_age_int == FALSE)){
-    f <- expr(!!sym(sna_measure) + 0.00001 ~ z.(age_mid_year) + sex + z.(avg_rank) + (1|chimp_id))
-  }
-  if(all(net_sex == "any_combo" & subj_sex == "M" | net_sex == "male")){
-    f <- expr(!!sym(sna_measure) + 0.00001 ~ z.(age_mid_year) + z.(avg_rank) + (1|chimp_id))
-  }
-  if(all(net_sex == "any_combo" & subj_sex == "F" | net_sex == "female")){
-    f <- expr(!!sym(sna_measure) + 0.00001 ~ z.(age_mid_year) + z.(avg_rank) + z.(prop_cyc) + (1|chimp_id))
-  }
-  
-  #data
-  if(subj_sex == "both"){ subj_sex1 <- c("M", "F")} else {subj_sex1 <- subj_sex}
-  
-  d <- data %>%
-    filter(behavior == beh, network_sex == net_sex, sex %in% subj_sex1)
-  #model with error handling
-  mod <- tryCatch({
-    mod <- glmer(f, family = Gamma(link = "log") , data = d)
-  }, warning = function(w) {
-    mod <- "Warning: model does not converge, max|grad|"
-  }, error = function(e) {
-    mod <- "Error: model does not converge" 
-  }) #holy fuck it worked!!!!! I finally figured out tryCatch syntax...8o http://mazamascience.com/WorkingWithData/?p=912
-  
-  if(all(!inherits(mod, "character") & summary == T)){
-    mod <- summary(mod)
-  }
-  
-  
-  return(mod)
-  
-}
-
-
-#modeling every sna measure -------------
+# modeling every sna measure -------------
 # function filters the network behavior, the network sex, and the sex of network members if a mixed sex network
 # then models the relationships between sna measures and network sex and subj sex specific predictor variables
 
 age_sex_fun_all <- function(data, 
                             beh = c("total_grooming", "prox", "grooming"), 
-                            net_sex = "any_combo",
+                            net_sex = c("any_combo", "female","male"),
                             subj_sex = NULL,
                             sex_age_int = FALSE, summary = TRUE){
   
+  library(lme4)
   #scale shorthand function
   z. <- function(x) scale(x)
   
@@ -119,10 +71,20 @@ age_sex_fun_all <- function(data,
     }, error = function(e) {
       mod <- "Error: model does not converge" 
             }) #holy fuck it worked!!!!! I finally figured out tryCatch syntax...8o http://mazamascience.com/WorkingWithData/?p=912
+
+    # if max|grad reached, then change integration method - give model more points to integrate random effects over
+    # https://stats.stackexchange.com/questions/77313/why-cant-i-match-glmer-family-binomial-output-with-manual-implementation-of-g
+    if(inherits(mod, "character")){
+      if(grepl("max", mod)){
+        mod <- glmer(f, family = Gamma(link = "log") , data = d)
+        mod <- update(mod, nAGQ = 2) 
+      }
+      
+    }
+
     if(all(!inherits(mod, "character") & summary == T)){
       mod <- summary(mod)
     }
-    
     #storage
     mods[[i]] <- mod
   }
@@ -131,12 +93,69 @@ age_sex_fun_all <- function(data,
   
 }
 
-#peek in modeled data set ------
+# modeling function for single sna measure ------
+age_sex_fun_single <- function(data, sna_measure = c("bt", "ec", "deg", "trans", "deg_in", "deg_out"), 
+                               beh = c("total_grooming", "prox"), 
+                               net_sex = c("any_combo", "female", "male"), 
+                               subj_sex = NULL,
+                               sex_age_int = FALSE, summary = FALSE){
+  
+  #scale shorthand function
+  z. <- function(x) scale(x)
+  #model expressions
+  #both sexes 
+  if(all(net_sex == "any_combo" & subj_sex == "both" & sex_age_int == TRUE)){
+    f <- expr(!!sym(sna_measure) + 0.00001 ~ z.(age_mid_year) + sex + z.(avg_rank) + sex*z.(age_mid_year) + (1|chimp_id))
+  }
+  if(all(net_sex == "any_combo" & subj_sex == "both" & sex_age_int == FALSE)){
+    f <- expr(!!sym(sna_measure) + 0.00001 ~ z.(age_mid_year) + sex + z.(avg_rank) + (1|chimp_id))
+  }
+  if(all(net_sex == "any_combo" & subj_sex == "M" | net_sex == "male")){
+    f <- expr(!!sym(sna_measure) + 0.00001 ~ z.(age_mid_year) + z.(avg_rank) + (1|chimp_id))
+  }
+  if(all(net_sex == "any_combo" & subj_sex == "F" | net_sex == "female")){
+    f <- expr(!!sym(sna_measure) + 0.00001 ~ z.(age_mid_year) + z.(avg_rank) + z.(prop_cyc) + (1|chimp_id))
+  }
+  
+  #data
+  if(subj_sex == "both"){ subj_sex1 <- c("M", "F")} else {subj_sex1 <- subj_sex}
+  
+  d <- data %>%
+    filter(behavior == beh, network_sex == net_sex, sex %in% subj_sex1)
+  #model with error handling
+  mod <- tryCatch({
+    mod <- glmer(f, family = Gamma(link = "log") , data = d)
+  }, warning = function(w) {
+    mod <- "Warning: model does not converge, max|grad|"
+  }, error = function(e) {
+    mod <- "Error: model does not converge" 
+  }) # I finally figured out tryCatch syntax...8o http://mazamascience.com/WorkingWithData/?p=912
+  
+# if max|grad reached, then change integration method - give model more points to integrate random effects over
+  # https://stats.stackexchange.com/questions/77313/why-cant-i-match-glmer-family-binomial-output-with-manual-implementation-of-g
+  if(grepl("max", mod)){
+    mod <- glmer(f, family = Gamma(link = "log") , data = d)
+    mod <- update(mod, nAGQ = 2) 
+  }
+  
+  if(all(!inherits(mod, "character") & summary == T)){
+    mod <- summary(mod)
+  }
+  
+  
+  return(mod)
+  
+}
+
+
+# peek in modeled data set ------
 
 peep_dataset <- function(data, 
                          beh = c("total_grooming", "prox", "grooming"), 
-                         net_sex = "any_combo",
+                         net_sex = c("any_combo", "female", "male"),
                         subj_sex = NULL){
+  
+  filter <- dplyr::filter
   
   # assign a subj_sex if is NULL
   # subj_sex defaults to both if only a mixed network, net_sex = "any_combo", is specified
@@ -152,7 +171,8 @@ peep_dataset <- function(data,
     filter(behavior == beh, network_sex == net_sex, sex %in% subj_sex1)
   
   zeros <- dat %>%
-    apply(., 2, function(x) sum(x == 0))
+    select_if(is.numeric) %>%
+    apply(., 2, function(x) sum(x == 0, na.rm = T))
   
   if(subj_sex == "both"){
     desc <- dat %>%
@@ -169,7 +189,7 @@ peep_dataset <- function(data,
   
 }
 
-#extract coefficient function - mostly for randomizations ------
+# extract coefficient function - mostly for randomizations ------
 
 # ex_coef <- function(m, coef = c("age", "sex")){
 #   if( m == "Error: model does not converge"){
@@ -185,32 +205,22 @@ peep_dataset <- function(data,
 #   return(b)
 # } 
 #figure out conditional lapply or just make loop for extracting
-# REWRITE FOR VARIOUS MODEL STRUCTURES... 
-# mixed both sexes
-# mixed female
-# mixed male
-# same female
-# same male
 
-ex_coef <- function(mod_list, coef = c("age", "sex", "int")){
+ex_coef <- function(mod_list, pred = c("age", "sex", "rank", "prop_cyc", "int")){
   
-  coef_list <- vector("list", length = 4)
-  names(coef_list) <- paste(names(mod_list), coef, sep = "_")
+  coef_list <- vector("list", length = 4) # one for each network measure
+  names(coef_list) <- paste(names(mod_list), pred, sep = "_")
+  if(pred == "int"){pred <- ":"} # for grep rownames in loop
+  
   
   for(k in 1:4){
     m <- mod_list[[k]]
     
     if(inherits(m, "summary.merMod")){
-
-        if(coef == "age"){
-          b <- coef(m)[2,1]
-        }
-        if(coef == "sex"){
-          b <- coef(m)[3,1]
-        }
-      if(coef == "int"){
-        b <- coef(m)[4,1]
-      }
+    cs <- coef(m) %>% data.frame()#summary
+    
+    b <- cs %>% slice(grep(pred, rownames(.))) %>%
+      .[1,1] #always chose the first row (when are more than 2, e.g. age and sex alone and in interaction) and first col bc = est
       }
     
     if( is.character(m)){
